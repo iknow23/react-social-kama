2da71899f2051b8dd12cfd70d64e9b00749ee3ef
Aynur Vafin <aynur.vafin@simbirsoft.com>
2020-05-27 12:07:49 +0300

    '54'

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import { Route } from 'react-router-dom';
+import UsersContainer from './components/Users/UsersContainer';
 
 const App = (props) => {
     return (
@@ -12,9 +13,10 @@ const App = (props) => {
                 <Header />
                 <Navbar />
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' render={ () => <DialogsContainer /> }/>
-
                     <Route path='/profile' render={ () => <Profile /> }/>
+                    <Route path='/dialogs' render={ () => <DialogsContainer /> }/>
+                    <Route path='/users' render={ () => <UsersContainer /> }/>
+
                 </div>
             </div>
         )
---
+++ src/components/Users/Users.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './Users.module.css';
+import * as axios from 'axios';
+import usersPhoto from '../../assets/images/user.png';
+
+class Users extends React.Component {
+    componentDidMount() {
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => (
+                        <div key={u.id}>
+                            <div>
+                                <div>
+                                    <img src={u.photos.small !== null ? u.photos.small : usersPhoto} className={styles.userPhoto} />
+                                </div>
+                                <div>
+                                    {u.followed ? <button onClick={() => this.props.unfollow(u.id)}>Follow</button> : <button onClick={() => this.props.follow(u.id)}>Unfollow</button>}
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <div>{u.name}</div>
+                                    <div>{u.status}</div>
+                                </div>
+                                <div>
+                                    <div>{'u.location.country'}</div>
+                                    <div>{'u.location.city'}</div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Users;
