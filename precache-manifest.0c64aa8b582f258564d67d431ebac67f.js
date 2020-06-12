e7ce529ea22bdbfc6aa319a2909210027a703448
Aynur Vafin <aynur.vafin@simbirsoft.com>
2020-06-12 13:06:57 +0300

    Updates

---
+++ precache-manifest.0c64aa8b582f258564d67d431ebac67f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "72e0400e73507e732ba2",
+    "url": "/react-social-kama/static/js/2.72e0400e.chunk.js"
+  },
+  {
+    "revision": "1b0b8a590107cd3c556d",
+    "url": "/react-social-kama/static/css/main.92a039aa.chunk.css"
+  },
+  {
+    "revision": "ca5fed7479e919314c3aaddba9eddae3",
+    "url": "/react-social-kama/static/media/loader.ca5fed74.svg"
+  },
+  {
+    "revision": "f84665fbf1b19bd4dfa5",
+    "url": "/react-social-kama/static/js/1.f84665fb.chunk.js"
+  },
+  {
+    "revision": "f8a15e74d9b85e04019c7efd5ae76dff",
+    "url": "/react-social-kama/static/media/user.f8a15e74.png"
+  },
+  {
+    "revision": "1b0b8a590107cd3c556d",
+    "url": "/react-social-kama/static/js/main.1b0b8a59.chunk.js"
+  },
+  {
+    "revision": "9d4d9e7e40215c35f70a",
+    "url": "/react-social-kama/static/js/3.9d4d9e7e.chunk.js"
+  },
+  {
+    "revision": "2b004a0796a0dcbc8eda",
+    "url": "/react-social-kama/static/js/runtime~main.2b004a07.js"
+  },
+  {
+    "revision": "72e0400e73507e732ba2",
+    "url": "/react-social-kama/static/css/2.3aa9466c.chunk.css"
+  },
+  {
+    "revision": "f84665fbf1b19bd4dfa5",
+    "url": "/react-social-kama/static/css/1.1e651645.chunk.css"
+  },
+  {
+    "revision": "a5d099a6a6496a51d9ef534f372673f3",
+    "url": "/react-social-kama/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-social-kama/precache-manifest.a2686b9285dbc6a602b53b5e31f3d894.js"
+  "/react-social-kama/precache-manifest.0c64aa8b582f258564d67d431ebac67f.js"
 );
 
 workbox.clientsClaim();
